572d038169018d1cb0382f9f6084a326a48b9468
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-05-01 21:17:27 -0500

    test

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,30 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
+  mode: "production",
+  entry: "./js/dashboard_main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
+              name: "[name].[ext]",
+              outputPath: "images/",
             },
           },
         ],
+      },
+      {
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: {
+            progressive: true,
+          },
+        },
       },
     ],
   },
