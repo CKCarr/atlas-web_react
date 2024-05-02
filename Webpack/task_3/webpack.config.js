1e60b3c757b5695c5061596b02b7b496b30d8d96
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-05-01 23:07:10 -0500

    task 3

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,59 +1,80 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
     header: "./modules/header/header.js",
-    body: './modules/header/body.js',
-    footer: './modules/header/footer.js',
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "commons",
+          chunks: "all",
+        },
+      },
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            minimize: true,
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
+              name: "[name].[ext]",
+              outputPath: "images/",
             },
           },
         ],
       },
+      {
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: {
+            progressive: true,
+          },
+        },
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-      inject: 'body',
-    }),
-    new MiniCssExtractPlugin({
-      filename: './css/[name].css',
-    }),
-    new CleanWebpackPlugin(
-      {
-        cleanStaleWebpackAssets: false,
-      }
-    ),
-  ],
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 8564,
   },
-  devTool: 'inline-source-map'
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin(),
+  ],
 };
