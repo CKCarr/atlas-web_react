eff5bd0af1ea32e0f75b9498ab8c3f96e19f3569
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-05-19 17:20:50 -0500

    task_4 checks

---
+++ react_state/task_4/dashboard/jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+
+module.exports = {
+  testEnvironment: "jest-environment-jsdom", // or js-dom?
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  snapshotSerializers: ["enzyme-to-json/serializer", "jest-aphrodite-react"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/config/setupTests.js"],
+};
