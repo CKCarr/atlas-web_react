8c0b02259ea628f265ae9376518c3884f2543d1a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-05-20 17:04:57 -0500

    task_1 complete

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,12 +1,25 @@
 // task_0/dashboard/src/schema/notifications.js
+
+import { normalize, schema } from "normalizr";
 import * as notificationsData from "../../notifications.json";
 
-// function that accepts userId as an argument and returns all notifications for that user from the notifications.json file.
-// returns list of all notifications for that user from the notifications.json file that matches the given userId
+// schema.Entity args (key, definition = {}, options = {})
 
-// console.log("Imported notifications data:", notificationsData);
-// this passes
+// define the schema user
+const user = new schema.Entity("users");
+// define the schema message
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+// define the schema notification
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
 
+// normalize the notificationsData with the notification schema
+export const normalizedData = normalize(notificationsData, [notification]);
+
+// function that accepts userId as an argument and returns notifications for that user from the notifications.json file.
+// returns list of all notifications for that author id that matches the given userId
 export const getAllNotificationsByUser = (userId) => {
   const notifications = notificationsData.default;
 
